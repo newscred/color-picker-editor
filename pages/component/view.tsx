6af9b748da71c3bef736333705ed0c81ffd666c1
Redwanul Haque <mdredwan.rh@gmail.com>
2023-08-29 14:59:24 +0600

    Use hex color type everywhere

--- pages/component/view.tsx
+++ pages/component/view.tsx
@@ -1,21 +1,17 @@
 import dynamic from "next/dynamic";
 import { useState, useRef, useEffect } from "react";
 import useHostChannel from "@/hooks/useHostChannel";
+import { Color } from "@/helpers/types";
 
 const ColorSummary = dynamic(() => import("@/components/ColorSummary"), {
   loading: () => <p>Loading...</p>,
 });
 
-const DEFAULT_COLOR = {
-  r: 0,
-  g: 0,
-  b: 0,
-  a: 1,
-};
+const DEFAULT_COLOR = "#000000";
 
 export default function Component() {
   const colorRef = useRef<HTMLDivElement>(null);
-  const [color, setColor] = useState(DEFAULT_COLOR);
+  const [color, setColor] = useState<Color | null>(null);
   const [config, setConfig] = useState<{ shade?: string }>({});
 
   const hostChannel = useHostChannel({
@@ -49,38 +45,25 @@ export default function Component() {
   };
 
   useEffect(() => {
-    if (color !== DEFAULT_COLOR) {
+    if (color) {
       return;
     }
 
-    const shade = config.shade ?? "none";
+    const shade = config.shade ?? "";
     if (shade == "red") {
-      setColor({
-        r: 255,
-        g: 0,
-        b: 0,
-        a: 1,
-      });
+      setColor("#ff0000");
     } else if (shade == "blue") {
-      setColor({
-        r: 0,
-        g: 0,
-        b: 255,
-        a: 1,
-      });
+      setColor("#0000ff");
     } else if (shade == "green") {
-      setColor({
-        r: 0,
-        g: 255,
-        b: 0,
-        a: 1,
-      });
+      setColor("#00ff00");
     }
   }, [color, config]);
 
+  const displayColor = color ?? DEFAULT_COLOR;
+
   return (
     <div ref={colorRef}>
-      <ColorSummary color={color} onClick={handleClick} />
+      <ColorSummary color={displayColor} onClick={handleClick} />
     </div>
   );
 }
