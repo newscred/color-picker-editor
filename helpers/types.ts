4457a6c4b1c720fc946b1b475eea2267000e95fc
Shadab <oneshadab@gmail.com>
2023-09-11 00:00:23 +0600

    Move types to common file

--- helpers/types.ts
+++ helpers/types.ts
@@ -3,3 +3,26 @@ export type Color = string;
 export type Config = {
   shade?: string;
 };
+
+export type HostMessage =
+  | { type: "connected" }
+  | {
+      type: "field-value";
+      data: { color: Color };
+    }
+  | {
+      type: "field-config";
+      data: Config;
+    };
+
+export type ComponentMessage =
+  | { type: "connect" }
+  | { type: "get:field-value" }
+  | { type: "get:field-config" }
+  | {
+      type: "set:field-value";
+      data: { color: Color };
+    }
+  | { type: "get:mode" }
+  | { type: "set:mode"; data: "view" | "edit" }
+  | { type: "set:style"; data: { height: string } };
