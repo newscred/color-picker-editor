af3ecbbb7be4a3ef1d430759950f7165bfdfa2f6
Redwanul Haque <mdredwan.rh@gmail.com>
2023-09-13 23:10:04 +0600

    Split the settings type into clear field types

--- pages/api/settings.ts
+++ pages/api/settings.ts
@@ -1,17 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Settings = {
-  [key: string]: {
-    label: string;
-    required?: boolean;
-    default?: string;
-  } & (
-    | { type: "dropdown"; options: Array<{ label: string; value: string }> }
-    | { type: "text"; maxLength?: number }
-  );
+type BaseField = {
+  label: string;
+  required?: boolean;
+  disabled?: boolean;
 };
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<Settings>) => {
+export type TextField = BaseField & {
+  type: "text";
+  maxLength?: number;
+  default?: string;
+};
+
+export type DropdownField = BaseField & {
+  type: "dropdown";
+  options: Array<{ label: string; value: string }>;
+  default?: string[] | string;
+  multivalued?: boolean;
+  creatable?: boolean;
+};
+
+export type Schema = {
+  [key: string]: DropdownField | TextField;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Schema>) => {
   res.status(200).json({
     shade: {
       type: "dropdown",
