fd068cbd4da79e7fd720d8ed2730cf072604dbd5
Redwanul Haque <mdredwan.rh@gmail.com>
2023-09-24 18:01:50 +0600

    Respond immediately instead of saving error message
    Co-authored-by: Shadman Shadab <oneshadab@gmail.com>

--- pages/api/validate.ts
+++ pages/api/validate.ts
@@ -11,16 +11,12 @@ const handler = async (
 
   const color = req.body.color;
 
-  let errorMessage = null;
   if (typeof color !== "string") {
-    errorMessage = "No 'color' hex string found";
-  } else if (!color.match(/^#[0-9a-f]{6}([0-9a-f]{2})?$/i)) {
-    errorMessage = `'${color}' is not a valid hex color string`;
+    return res.status(422).json({ message: "No 'color' hex string found" });
   }
-
-  if (errorMessage) {
-    res.status(422).json({ message: errorMessage });
-    return;
+  
+  if (!color.match(/^#[0-9a-f]{6}([0-9a-f]{2})?$/i)) {
+    return res.status(422).json({ message: `'${color}' is not a valid hex color string` });
   }
 
   res.status(200).json({ message: "OK" });
